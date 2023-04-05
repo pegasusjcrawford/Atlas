b8e6664ab212d79229412cdf8a9d6e4faf404dce
Pegasus J. Crawford II <pegasus.j.crawford@protonmail.com>
2023-04-04 22:54:58 -0700

    get the indexer to the point where it's connecting to mongodb & trying to start uprpc

--- src/Okeanos.Atlas.Indexer/Program.cs
+++ src/Okeanos.Atlas.Indexer/Program.cs
@@ -1,3 +1,5 @@
+using Okeanos.Atlas.Indexer.Extensions;
+
 namespace Okeanos.Atlas.Indexer
 {
     public class Program
@@ -5,8 +7,17 @@ namespace Okeanos.Atlas.Indexer
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.WebHost.ConfigureAppConfiguration(config =>
+            {
+                config.AddBlockcore("Atlas Indexer", args);
+            });
+
+            Startup.AddIndexerServices(builder.Services, builder.Configuration);
+        
             var app = builder.Build();
 
+            Startup.Configure(app, app.Environment);
             app.MapGet("/", () => "Hello World!");
 
             app.Run();
